ce44474a411caedada0b39c40f8c54828f6bf176
Alex Reinking <alex.reinking@gmail.com>
2018-11-18 02:23:56 -0800

    Making script more friendly

--- attack_dp_mnist.py
+++ attack_dp_mnist.py
@@ -3,6 +3,8 @@ import argparse
 import numpy as np
 import tensorflow as tf
 
+IMAGE_SIZE = 28
+
 
 def load_trained_mnist(model_path, inputs):
     importer = tf.train.import_meta_graph(model_path + '.meta', import_scope='mnist',
@@ -15,6 +17,13 @@ def load_trained_mnist(model_path, inputs):
     return output
 
 
+def compute_model_path(model_path: str) -> str:
+    ending = '.meta'
+    if model_path.endswith(ending):
+        model_path = model_path[:-len(ending)]
+    return model_path
+
+
 def main():
     parser = argparse.ArgumentParser(description='Run the trained MNIST model')
     parser.add_argument('--model_path', help='The path to the trained checkpoint (sans .meta)')
@@ -22,10 +31,14 @@ def main():
                         default=10)
     args = parser.parse_args()
 
+    model_path = compute_model_path(args.model_path)
+
     with tf.Session() as sess:
-        dummy_input = tf.constant(np.ones((args.num_tensors, 28 * 28)), dtype=tf.float32)
+        all_ones = np.ones((args.num_tensors, IMAGE_SIZE ** 2))
+        dummy_input = tf.constant(all_ones, dtype=tf.float32)
+
         init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-        mnist_output = load_trained_mnist(args.model_path, dummy_input)
+        mnist_output = load_trained_mnist(model_path, dummy_input)
         _, output = sess.run([init_op, mnist_output])
 
         print(output)
