6f67a9d90092bd9bb39aa51825803e7beb1c98fe
Elizabeth Yang <elizabeth_yang@berkeley.edu>
2018-11-22 01:33:43 -0500

    Put attack_dp_mnist.py code into Model.py

--- Model.py
+++ Model.py
@@ -2,28 +2,53 @@ import os
 
 import tensorflow as tf
 
+def load_trained_mnist(ckpt, inputs):
+    importer = tf.train.import_meta_graph(ckpt + '.meta', import_scope='mnist',
+                                          input_map={'dp_mnist_input': inputs})
+    output = tf.get_collection('dp_mnist_output')[0]
+
+    sess = tf.get_default_session()
+    importer.restore(sess, ckpt)
+
+    return output
+
+def compute_model_path(model_path: str) -> str:
+    ending = '.meta'
+    if model_path.endswith(ending):
+        model_path = model_path[:-len(ending)]
+    return model_path
+
 class Model:
-    def __init__(self, metagraph_path):
-        self.ckpt = os.path.join(metagraph_path, 'ckpt')
+    def __init__(self, model_path):
+        self.model_path = compute_model_path(model_path)
+        self.ckpt = os.path.join(self.model_path, 'ckpt')
         self.image_size = 28
         self.num_channels = 1
         self.num_labels = 10
 
-    def predict(self, input_tensor, sess_input):
-        # sess = tf.get_default_session()
-        # importer = tf.train.import_meta_graph(self.ckpt + '.meta', import_scope='mnist',
-        #                                       input_map={'dp_mnist_input': images})
-        # importer.restore(sess, self.ckpt)
-        # return tf.get_collection('logits')[0]
-
+    def predict(self, input_tensor):
         with tf.Session() as sess:
-            importer = tf.train.import_meta_graph(self.ckpt + '.meta', import_scope='mnist',
-	                                          input_map={'dp_mnist_input': input_tensor})
-            mnist_output = tf.get_collection('dp_mnist_output')[0]
-            sess.run(tf.global_variables_initializer())
-
-            importer.restore(sess, self.ckpt)
             init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-            _, output = sess.run([init_op, mnist_output])
+            _ = sess.run([init_op])
+            mnist_output = load_trained_mnist(self.ckpt, input_tensor)
+            output = sess.run([mnist_output])
+            return tf.convert_to_tensor(output[0])
+
+    # def predict(self, input_tensor):
+    #     # sess = tf.get_default_session()
+    #     # importer = tf.train.import_meta_graph(self.ckpt + '.meta', import_scope='mnist',
+    #     #                                       input_map={'dp_mnist_input': images})
+    #     # importer.restore(sess, self.ckpt)
+    #     # return tf.get_collection('logits')[0]
+
+    #     with tf.Session() as sess:
+    #         importer = tf.train.import_meta_graph(self.ckpt + '.meta', import_scope='mnist',
+    #                                               input_map={'dp_mnist_input': input_tensor})
+    #         mnist_output = tf.get_collection('dp_mnist_output')[0]
+    #         sess.run(tf.global_variables_initializer())
+
+    #         importer.restore(sess, self.ckpt)
+    #         init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    #         _, output = sess.run([init_op, mnist_output])
             
-            return output, sess_input
+    #         return tf.convert_to_tensor(output)
