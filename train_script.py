64b652342d4986e2b4b4ce787947337c3679a0f3
Elizabeth Yang <elizabeth_yang@berkeley.edu>
2018-11-25 17:59:35 -0500

    Fixes to train script, makes training amortized.

--- train_script.py
+++ train_script.py
@@ -2,16 +2,18 @@ import os
 import subprocess
 import tensorflow as tf
 
-pcaInputs = [('no_pca', '0'), ('pca', '60')]
-epsInputs = ['0'] #, '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1']
-				# ['0.01', '0.02', '0.03', '0.04', '0.05', '0.06', '0.07', '0.08', '0.09']
-numTrainingStepsInputs = ['10']
+# pcaInputs = [('no_pca', '0'), ('pca', '60')]
+pcaInputs = [('pca', '60')]
+epsInputs = ['0', '0.1', '0.2', '0.5', '1', '2', '4'] 
+# epsInputs = ['0.1']
+# numTrainingStepsInputs = ['600']
+numTrainingStepsInputs = ['600']
 
 for pca in pcaInputs:
 	for eps in epsInputs:
 		for numTrainingSteps in numTrainingStepsInputs:
 			# If the save directory doesn't exist, create it
-			savePath = 'trained_models/%s_eps_%s_%s' % (pca[0], eps, numTrainingSteps)
+			savePath = 'trained_models/%s_eps_%s_%s_amortized' % (pca[0], eps, numTrainingSteps)
 			if not os.path.isdir(savePath):
 				os.mkdir(savePath)
 
@@ -21,9 +23,12 @@ for pca in pcaInputs:
 				cmd.append('--training_data_path=dp_data/mnist_train.tfrecord')
 				cmd.append('--eval_data_path=dp_data/mnist_test.tfrecord')
 				cmd.append('--save_path=%s' % savePath)
-				cmd.append('eps=%s' % eps)
+				cmd.append('--eps=%s' % eps)
+				cmd.append('--end_eps=%s' % eps)
+				cmd.append('--sigma=0')
 				cmd.append('--num_training_steps=%s' % numTrainingSteps)
 				cmd.append('--projection_dimensions=%s' % pca[1])
+				cmd.append('--accountant_type=Amortized')
 
 				subprocess.check_output(cmd)
 				print('Checkpoint written to %s' % savePath)
